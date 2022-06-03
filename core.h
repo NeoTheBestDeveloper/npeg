f26426ff9014f8d0b8352816c1a8c2849bd3c44a
NeoTheBestDeveloper <neothebestdeveloper@protonmail.com>
2022-06-03 20:42:27 +0300

    Feat: add verticle photo mirroring for pbm extension.

--- core.h
+++ core.h
@@ -3,5 +3,5 @@
 enum image_format { PBM, PGM, PPM, BMP, UNKNOWN_FORMAT };
 enum image_encoding { ASCII, RAW, UNKNOWN_ENCODING };
 
-image_format parse_image_format(const FILE *image);
-image_encoding parse_image_encoding(const FILE *image);
+image_format parse_image_format(FILE *image);
+image_encoding parse_image_encoding(FILE *image);
--- pbm_image.h
+++ pbm_image.h
@@ -5,9 +5,13 @@ typedef struct pbm_image_t {
     unsigned char **image_data;
 } pbm_image;
 
-pbm_image create_pbm_image(const FILE *image);
+enum direction { VERTICAL, HORIZONTAL };
+
+pbm_image create_pbm_image(FILE *image);
 void free_pbm_image(pbm_image *image);
 
 static void _parse_pbm_specific_metadata(pbm_image *image_metadata,
-                                         const FILE *image);
-static char **_read_image_data(const FILE *image);
+                                         FILE *image);
+static unsigned char **_read_image_data(FILE *image);
+void reverse_photo(pbm_image *image, const int _direction);
+void dump_photo(pbm_image *image, const char *path);
