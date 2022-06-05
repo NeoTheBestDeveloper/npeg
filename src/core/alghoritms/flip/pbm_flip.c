a727167038da580e044b43b4acfb47b6ec6e381d
NeoTheBestDeveloper <neothebestdeveloper@protonmail.com>
2022-06-05 16:24:43 +0300

    Refactoring: rename photo->image and some little name fixes.

--- src/core/alghoritms/flip/pbm_flip.c
+++ src/core/alghoritms/flip/pbm_flip.c
@@ -1,24 +1,25 @@
-#include <math.h>
-#include <stdint.h>
 #include <stdio.h>
 
 #include "../../image_formats/pbm_image.h"
 
-static unsigned char _reverse_bits(unsigned char n) {
+static unsigned char _reverse_bits(unsigned char num) {
     // Reverse bits string.
-    unsigned int NO_OF_BITS = sizeof(n) * 8;
-    unsigned char mirror_num = 0;
+    unsigned int NO_OF_BITS = sizeof(num) * 8;
+    unsigned char reversed_num = 0;
+
     for (int i = 0; i < NO_OF_BITS; i++) {
-        if ((n & (1 << i)))
-            mirror_num |= 1 << ((NO_OF_BITS - 1) - i);
+        if ((num & (1 << i)))
+            reversed_num |= 1 << ((NO_OF_BITS - 1) - i);
     }
-    return mirror_num;
+    return reversed_num;
 }
 
-static void _vertical_flip_ascii_photo(pbm_image *image) {
+static void _vertical_flip_ascii_image(pbm_image *image) {
     for (int i = 0; i < image->height; i++) {
         for (int j = 0; j < image->bytes_width / 2; j++) {
             unsigned char tmp = image->image_data[i][j];
+
+            // just swap pixels from different edges of the image.
             image->image_data[i][j] =
                 image->image_data[i][image->bytes_width - j - 1];
             image->image_data[i][image->bytes_width - j - 1] = tmp;
@@ -28,10 +29,12 @@ static void _vertical_flip_ascii_photo(pbm_image *image) {
 
 // TODO: when bytes count is not multiple 8 it fill them and when I mirror image
 // TODO: photo is spoiled.
-static void _vertical_flip_raw_photo(pbm_image *image) {
+static void _vertical_flip_raw_image(pbm_image *image) {
     for (int i = 0; i < image->height; i++) {
         for (int j = 0; j < image->bytes_width / 2; j++) {
             unsigned char tmp = image->image_data[i][j];
+
+            // just swap pixels from different edges of the image.
             image->image_data[i][j] =
                 _reverse_bits(image->image_data[i][image->bytes_width - j - 1]);
             image->image_data[i][image->bytes_width - j - 1] =
@@ -41,11 +44,10 @@ static void _vertical_flip_raw_photo(pbm_image *image) {
 }
 
 void vertical_pbm_flip(pbm_image *image) {
-    if (image->encoding == RAW) {
-        _vertical_flip_raw_photo(image);
-    } else {
-        _vertical_flip_ascii_photo(image);
-    }
+    if (image->encoding == RAW)
+        _vertical_flip_raw_image(image);
+    else
+        _vertical_flip_ascii_image(image);
 }
 
 void horizontal_pbm_flip(pbm_image *image) { printf("Not implemented"); }
