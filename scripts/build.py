bdc411bd9040fa8861f1fc7dfbaf9598aa454672
NeoTheBestDeveloper <neothebestmain@protonmail.com>
2023-03-20 22:47:25 +0300

    Dev: add intalling clang and meson for github workflows.

--- scripts/build.py
+++ scripts/build.py
@@ -1,17 +1,19 @@
 from os import system
 
+CC = "clang"
+
 
 def build_dev_lib() -> None:
     """Build dev core library and copy shared object file into python npeg catalog."""
-    system("meson setup --buildtype=debug debug_build")
-    system("meson compile -j 8 -C debug_build")
+    system(f"CC={CC} meson setup --buildtype=debug debug_build")
+    system(f"CC={CC} meson compile -j 8 -C debug_build")
     system("cp debug_build/libnpeg.so npeg/bindings/_c_npeg.so")
 
 
 def build_release_lib() -> None:
     """Build release core library and copy shared object file into python npeg catalog."""
-    system("meson setup --buildtype=release release_build -Db_sanitize=none")
-    system("meson compile -j 8 -C release_build")
+    system(f"CC={CC} meson setup --buildtype=release release_build -Db_sanitize=none")
+    system(f"CC={CC} meson compile -j 8 -C release_build")
     system("strip release_build/libnpeg.so")
     system("cp release_build/libnpeg.so npeg/bindings/_c_npeg.so")
 
