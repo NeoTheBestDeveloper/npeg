f1839e4097dcdd31d89ba8e3b0f44f9be5321511
NeoTheBestDeveloper <neothebestmain@protonmail.com>
2023-03-21 12:16:02 +0300

    feat: add windows support

--- npeg/bindings/__init__.py
+++ npeg/bindings/__init__.py
@@ -1,5 +1,6 @@
-from ctypes import CDLL, POINTER, c_char_p, c_float, c_int, c_void_p
+from sys import platform
 from pathlib import Path
+from ctypes import CDLL, POINTER, c_char_p, c_float, c_int, c_void_p
 
 from .matrix_struct import MatrixStruct, MatrixType
 from .img_result_struct import ImgResultStruct, ImgResultType
@@ -19,32 +20,37 @@ __all__ = (
     "npeg_core",
 )
 
-LIB_PATH = Path(__file__).parent.joinpath("_npeg_core.so")
+if platform in ("linux", "linux2"):
+    LIB_PATH = Path(__file__).parent.joinpath("_npeg_core.so")
+elif platform == "win32":
+    LIB_PATH = Path(__file__).parent.joinpath("_npeg_core.dll")
+else:
+    raise OSError(f"This platform '{platform}' is not supported.")
 
 
 def load_lib() -> CDLL:
-    dll = CDLL(LIB_PATH.as_posix())
+    lib = CDLL(LIB_PATH.as_posix())
 
-    dll.cast_void_to_img.restype = POINTER(ImgStruct)
-    dll.cast_void_to_img.argtypes = (c_void_p,)
+    lib.cast_void_to_img.restype = POINTER(ImgStruct)
+    lib.cast_void_to_img.argtypes = (c_void_p,)
 
-    dll.img_read.restype = ImgResultStruct
-    dll.img_read.argtypes = (c_char_p,)
+    lib.img_read.restype = ImgResultStruct
+    lib.img_read.argtypes = (c_char_p,)
 
-    dll.img_write.restype = ImgResultStruct
-    dll.img_write.argtypes = (POINTER(ImgStruct), c_char_p)
+    lib.img_write.restype = ImgResultStruct
+    lib.img_write.argtypes = (POINTER(ImgStruct), c_char_p)
 
-    dll.img_copy.restype = POINTER(ImgStruct)
-    dll.img_copy.argtypes = (POINTER(ImgStruct),)
+    lib.img_copy.restype = POINTER(ImgStruct)
+    lib.img_copy.argtypes = (POINTER(ImgStruct),)
 
-    dll.img_free.argtypes = (POINTER(ImgStruct),)
+    lib.img_free.argtypes = (POINTER(ImgStruct),)
 
-    dll.img_rotate.argtypes = (POINTER(ImgStruct), c_float, c_int)
+    lib.img_rotate.argtypes = (POINTER(ImgStruct), c_float, c_int)
 
-    dll.cast_img_to_pbm.restype = POINTER(PbmImgStruct)
-    dll.cast_img_to_pbm.argtypes = (POINTER(ImgStruct),)
+    lib.cast_img_to_pbm.restype = POINTER(PbmImgStruct)
+    lib.cast_img_to_pbm.argtypes = (POINTER(ImgStruct),)
 
-    return dll
+    return lib
 
 
 npeg_core = load_lib()
--- scripts/build.py
+++ scripts/build.py
@@ -9,6 +9,10 @@ def build_dev_lib() -> None:
     system(f"CC={CC} meson compile -j 8 -C debug_build")
     system("cp debug_build/libnpeg-core.so npeg/bindings/_npeg_core.so")
 
+    system(f"CC={CC} meson setup --cross-file=x86_64-w64-mingw32.txt --buildtype=debug debug_win_build")
+    system(f"CC={CC} meson compile -j 8 -C debug_win_build")
+    system("cp debug_win_build/libnpeg-core.dll npeg/bindings/_npeg_core.dll")
+
 
 def build_release_lib() -> None:
     """Build release core library and copy shared object file into python npeg catalog."""
@@ -16,6 +20,14 @@ def build_release_lib() -> None:
     system(f"CC={CC} meson compile -j 8 -C release_build")
     system("strip release_build/libnpeg-core.so")
     system("cp release_build/libnpeg-core.so npeg/bindings/_npeg_core.so")
+
+    system(
+        f"CC={CC} meson setup --cross-file=x86_64-w64-mingw32.txt"
+        "--buildtype=release release_win_build -Db_sanitize=none"
+    )
+    system(f"CC={CC} meson compile -j 8 -C release_win_build")
+    system("/usr/bin/x86_64-w64-mingw32-strip release_win_build/libnpeg-core.dll")
+    system("cp release_win_build/libnpeg-core.dll npeg/bindings/_npeg_core.dll")
 
 
 def build_pkg() -> None:
